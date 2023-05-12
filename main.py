2a9bdedc29386a807cd36fd7099d3868f64cb29d
Grachev Den <2717087@mail.ru>
2023-05-12 14:57:45 +0500

    " file name.py change name func sum a+b"

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import fucs
-a= input("Введите число a: ")
-b= input("Введите число b: ")
-sum_ = fucs.sum(a, b)
+a = input("Введите число a: ")
+b = input("Введите число b: ")
+sum_ = fucs.sum_two_numbers(a, b)
 print(sum_)
+
